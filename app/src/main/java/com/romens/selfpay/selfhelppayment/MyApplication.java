7a9fd7c5b8837eceb1833ca6798c4ad0bdc6fb78
merlin <1178091129@qq.com>
2018-04-24 16:52:28 +0800

    更新代码

---
+++ app/src/main/java/com/romens/selfpay/selfhelppayment/MyApplication.java
@@ -0,0 +1,11 @@
+package com.romens.selfpay.selfhelppayment;
+
+
+import com.romens.erp.library.LibraryApplication;
+
+/**
+ * Created by mMerlin on 2018/4/24.
+ */
+
+public class MyApplication extends LibraryApplication {
+}
