39ce50e4cb736c900e3b1a50c25e2c3a046c6cb5
lm <1573282877@qq.com>
2018-04-27 16:39:11 +0800

    修改商品列表item

--- app/src/main/java/com/romens/selfpay/selfhelppayment/ui/AlipayFailPayActivity.java
+++ app/src/main/java/com/romens/selfpay/selfhelppayment/ui/AlipayFailPayActivity.java
@@ -31,14 +31,14 @@ public class AlipayFailPayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FrameLayout content=new FrameLayout(this);
         setContentView(content);
-//        PayResultCell payResultCell=new PayResultCell(this);
-//        payResultCell.setValue("支付失败");
-//        payResultCell.setStateImge(R.drawable.ic_error_black_36dp);
-//        content.addView(payResultCell, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT,LayoutHelper.WRAP_CONTENT,Gravity.TOP, 20,36,20,20));
-        GoodsListCell cell=new GoodsListCell(this);
-        cell.setCount(1);
-        cell.setValue("爱他美婴儿奶粉800克",R.drawable.ic_launcher,"¥1500",null);
-        content.addView(cell,LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT,LayoutHelper.WRAP_CONTENT));
+        PayResultCell payResultCell=new PayResultCell(this);
+        payResultCell.setValue("支付失败");
+        payResultCell.setStateImge(R.drawable.ic_error_black_36dp);
+        content.addView(payResultCell, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT,LayoutHelper.WRAP_CONTENT,Gravity.TOP, 20,36,20,20));
+//        GoodsListCell cell=new GoodsListCell(this);
+//        cell.setCount(1);
+//        cell.setValue("爱他美婴儿奶粉800克",R.drawable.ic_launcher,"¥1500",null);
+//        content.addView(cell,LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT,LayoutHelper.WRAP_CONTENT));
 
         TextView hintView=new TextView(this);
         hintView.setTextColor(0xff212121);
